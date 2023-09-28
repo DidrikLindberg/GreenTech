2c4df07d1240e1e0e907861901b1d1baf00ab639
GitDidrik <lindberg.didrik@gmail.com>
2023-09-28 11:21:06 -0700

    react-dom update to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import { createRoot } from "react-dom";
-
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = document.getElementById("root");
-
-
-createRoot(root).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-
-      <App />
-
+    <App />
   </React.StrictMode>
 );
 
