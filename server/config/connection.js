19fc746a06bcf2b1fdc48d38345f523daf7dcc00
GitDidrik <lindberg.didrik@gmail.com>
2023-09-28 09:11:57 -0700

    models, scemas, utils and seeds

---
+++ server/config/connection.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/greentechDB';
+
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+  console.error('MongoDB connection error:', error);
+});
+
+db.once('open', () => {
+  console.log('Connected to MongoDB.');
+  console.log(db.name);
+});
+
+module.exports = db;
