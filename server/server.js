12467cecf5be35cdb3e24db3fc01f866c3e0e5d1
GitDidrik <lindberg.didrik@gmail.com>
2023-09-28 10:34:59 -0700

    deploy mongodb

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 
 const { authMiddleware } = require("./utils/auth");
 const { typeDefs, resolvers } = require("./schemas");
-// const db = require("./config/connection");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -40,42 +40,38 @@ app.get("/", (req, res) => {
     server.applyMiddleware({ app });
   };
   
-//   db.on("error", console.error.bind(console, "connection error:"));
+  db.on("error", console.error.bind(console, "connection error:"));
   
-//   db.once("open", function () {
-//     console.log("We are connected to the database!");
-//     checkConnectionState();
+  db.once("open", function () {
+    console.log("We are connected to the database!");
+    checkConnectionState();
   
-//     app.listen(PORT, () => {
-//       console.log(`API server running on port ${PORT}!`);
-//       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-//     });
-//   });
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
 
-app.listen(PORT, () => {
-          console.log(`API server running on port ${PORT}!`);
-          console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-        });
-  
-//   function checkConnectionState() {
-//     switch (db.readyState) {
-//       case 0:
-//         console.log("Mongoose connection state: disconnected");
-//         break;
-//       case 1:
-//         console.log("Mongoose connection state: connected");
-//         break;
-//       case 2:
-//         console.log("Mongoose connection state: connecting");
-//         break;
-//       case 3:
-//         console.log("Mongoose connection state: disconnecting");
-//         break;
-//       default:
-//         console.log("Mongoose connection state: unknown");
-//         break;
-//     }
-//   }
+
+  function checkConnectionState() {
+    switch (db.readyState) {
+      case 0:
+        console.log("Mongoose connection state: disconnected");
+        break;
+      case 1:
+        console.log("Mongoose connection state: connected");
+        break;
+      case 2:
+        console.log("Mongoose connection state: connecting");
+        break;
+      case 3:
+        console.log("Mongoose connection state: disconnecting");
+        break;
+      default:
+        console.log("Mongoose connection state: unknown");
+        break;
+    }
+  }
   
   startApolloServer();
   
