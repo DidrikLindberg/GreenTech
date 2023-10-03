a63153f2b8606c43c7f88437480ac173ea94a956
GitDidrik <lindberg.didrik@gmail.com>
2023-10-03 14:14:09 -0700

    revert

--- server/server.js
+++ server/server.js
@@ -25,7 +25,6 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 }
 
 
